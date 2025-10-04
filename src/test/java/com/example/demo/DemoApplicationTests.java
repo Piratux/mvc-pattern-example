ab5a2f0e874008ed0c10b52f8c9b1832d7b3a83c
Piratux <58703216+Piratux@users.noreply.github.com>
2025-10-04 21:17:12 +0300

    Update tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,8 +7,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,7 +26,7 @@ class DemoApplicationTests {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private CarService service;
 
     CarView createCar() {
